507e46c0626f51f2f7559a0c1785282df843e9bc
khoaredbull <89445257+khoaredbull@users.noreply.github.com>
2021-09-08 22:27:51 +0700

    sua lai

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
+
+$('.modal-item').slick({
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+});
+
 $('.click-me').click(function() {
-  $('.modal').css('display', 'flex');
+  $('.modal').removeClass('modan');
+  setTimeout(function() {
+    $('.modal-overlay, .modal').css('display', 'block');
+  }, 800);
+  $('.modal-item').slick('refresh');
 });
+
 $('.close').click(function() {
-  $('.modal').toggle();
-});
-$('.register').click(function() {
-  $('.modal-body').toggle();
+  $('.modal').addClass('modan');
+  setTimeout(function() {
+    $('.modal-overlay, .modal').css('display', 'none');
+  }, 800);
 });
 
+$('.btn-login').on('click', function(event) {
+    event.preventDefault();
+    $('.modal-item').slick('slickNext'); 
+});
 
+$('.btn-register').on('click', function(event) {
+    event.preventDefault();
+    $('.modal-item').slick('slickPrev'); 
+});
